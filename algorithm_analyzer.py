34e5e5d3fe43993b4f4452b6cd54c175a8c2f03c
Adam Molnar <molnar.adam995@gmail.com>
2020-09-20 14:54:15 +0200

    Create shell script for runnings

--- algorithm_analyzer.py
+++ algorithm_analyzer.py
@@ -10,8 +10,9 @@ Created on Mon Sep 14 19:43:17 2020
 import pandas as pd
 import os
 
-def algorithm_evaluation(file_url, result_url, k):
-    df = pd.read_csv(file_url)
+def algorithm_evaluation(file_name, result_url, k):
+    path = './improved_algorithm_result/big_graphs/' + str(k) + '/' + file_name + '.csv'
+    df = pd.read_csv(path)
     
     # random choice alg
     alg_time = 0
@@ -31,6 +32,11 @@ def algorithm_evaluation(file_url, result_url, k):
         base_alg_time = base_alg_time + t
     base_alg_time = base_alg_time / len(df)
     
+    greedy_alg_time = 0
+    for t in df['greedy_time']:
+        greedy_alg_time = greedy_alg_time + t
+    greedy_alg_time = greedy_alg_time / len(df)
+    
     alg_solution_dif = 0
     for index, row in df.iterrows():
         alg_solution_dif = alg_solution_dif + (row['correct_span_collector'] - row['algorithm_span_collector'])
@@ -46,33 +52,46 @@ def algorithm_evaluation(file_url, result_url, k):
         base_solution_dif = base_solution_dif + (row['correct_span_collector'] - row['algorithm_1_span_collector'])
     base_solution_dif = base_solution_dif / len(df)
     
+    greedy_solution_dif = 0
+    for index,row in df.iterrows():
+        greedy_solution_dif = greedy_solution_dif + (row['correct_span_collector'] - row['greedy_span_collector'])
+    greedy_solution_dif = greedy_solution_dif / len(df)
+    
     alg_time_list = list()
     improved_alg_time_list = list()
     base_alg_time_list = list()
+    greedy_time_list = list()
     alg_solution_dif_list = list()
     base_solution_dif_list = list()
     improved_alg_solution_dif_list = list()
+    greedy_solution_dif_list = list()
     
     alg_time_list.append(str(alg_time))
     improved_alg_time_list.append(str(improved_alg_time))
     base_alg_time_list.append(str(base_alg_time))
+    greedy_time_list.append(str(greedy_alg_time))
     alg_solution_dif_list.append(str(alg_solution_dif))
     base_solution_dif_list.append(str(base_solution_dif))
     improved_alg_solution_dif_list.append(str(improved_alg_solution_dif))
+    greedy_solution_dif_list.append(str(greedy_solution_dif))
         
     result_data = {
         'alg_time': alg_time_list,
         'improved_alg_time': improved_alg_time_list,
         'base_alg_time': base_alg_time_list,
+        'greedy_time': greedy_time_list,
         'alg_solution_dif': alg_solution_dif_list,
         'improved_alg_solution_dif': improved_alg_solution_dif_list,
-        'base_solution_dif': base_solution_dif_list
+        'base_solution_dif': base_solution_dif_list,
+        'greedy_solution_dif_list': greedy_solution_dif_list,
     }
     
     res_df = pd.DataFrame(result_data)
     
     if not os.path.exists('algorithm_evaluations'):
         os.makedirs('algorithm_evaluations')
-    if not os.path.exists('algorithm_evaluations/' + str(k)):
-        os.makedirs('algorithm_evaluations/' + str(k))
-    res_df.to_csv('./algorithm_evaluations/' + str(k) + '/' + str(result_url)  +'.csv', index=False)
+    if not os.path.exists('algorithm_evaluations/big_graphs'):
+        os.makedirs('algorithm_evaluations/big_graphs')
+    if not os.path.exists('algorithm_evaluations/big_graphs/' + str(k)):
+        os.makedirs('algorithm_evaluations/big_graphs/' + str(k))
+    res_df.to_csv('./algorithm_evaluations/big_graphs/' + str(k) + '/' + str(result_url) + '.csv', index=False)
--- new_algorithm_when_k_is_more.py
+++ new_algorithm_when_k_is_more.py
@@ -13,6 +13,7 @@ import pandas as pd
 import os
 import time
 import algorithm_analyzer as alg_analize
+import sys
 
 def compare_algorithms(k, sample_size, barabasi_albert_n, barabas_albert_m, result_file):
     basic_graph_collector = list()
@@ -28,25 +29,37 @@ def compare_algorithms(k, sample_size, barabasi_albert_n, barabas_albert_m, resu
     algorithm_1_span_collector = list()
     algorithm_1_edge_collector = list()
     algorithm_1_time = list()
+    greedy_span_collector = list()
+    greedy_edge_collector = list()
+    greedy_time = list()
     
     for index in range(sample_size):
+        global_start_time = time.time()
         t = graph.create_barabasi_albert_graph(barabasi_albert_n, barabas_albert_m)
         t_copy = t.copy()
         t_copy_2 = t.copy()
+        t_copy_3 = t.copy()
         basic_graph_collector.append(t.edges)
         
         basic_span = graph.calculate_number_of_spanning_trees(t)
         basic_span_collector.append(basic_span)
         
+        print(str(index+1) + '. graph is created')
+        print('time: ', time.time() - global_start_time)
         n = len(t.nodes())
         g_comp = graph.fully_connected_graph_from_list(n)
         dif = graph.difference(g_comp, t).edges(data='weight', default=1)
         dif = list(dif)
         dif_2 = dif.copy()
         dif_3 = dif.copy()
+        dif_4 = dif.copy()
+        
         correct_span = st_alg.graph_enumeration(t, dif, k, 8)
         correct_span_collector.append(correct_span[0])
         correct_edge_collector.append(correct_span[1])
+        
+        print('correct spanning tree no. : ', correct_span)
+        print('time: ', time.time() - global_start_time)
         
         edge_collector = list()
         start_time = time.time()
@@ -55,11 +68,17 @@ def compare_algorithms(k, sample_size, barabasi_albert_n, barabas_albert_m, resu
             dif_2.remove((edge[0],edge[1],1))
             t.add_edge(edge[0], edge[1])
             edge_collector.append((edge))
+        
+        print('random_select algorithm res: ', edge_collector)
+        print('time: ', time.time() - global_start_time)
             
         algorithm_span = graph.calculate_number_of_spanning_trees(t)
         algorithm_span_collector.append(algorithm_span)
         algorithm_edge_collector.append(edge_collector)
-        algorithm_time.append((time.time() - start_time)) 
+        algorithm_time.append((time.time() - start_time))
+        
+        print('random select span: ' , algorithm_span)
+        print('time: ', time.time() - global_start_time)
             
         improved_edge_collector = list()
         start_time = time.time()
@@ -68,11 +87,17 @@ def compare_algorithms(k, sample_size, barabasi_albert_n, barabas_albert_m, resu
             dif_3.remove((edge[0], edge[1], 1))
             t_copy_2.add_edge(edge[0], edge[1])
             improved_edge_collector.append(edge)
+            
+        print('improved algorithm res: ', improved_edge_collector)
+        print('time: ', time.time() - global_start_time)
         
         improved_algorithm_span = graph.calculate_number_of_spanning_trees(t_copy_2)
         improved_algorithm_span_collector.append(improved_algorithm_span)
         improved_algorithm_edge_collector.append(improved_edge_collector)
         improved_algorithm_time.append((time.time() - start_time))
+        
+        print('improved algorithm span: ', improved_algorithm_span)
+        print('time: ', time.time() - global_start_time)
         
         q = set(dif)
         start_time = time.time()
@@ -80,9 +105,23 @@ def compare_algorithms(k, sample_size, barabasi_albert_n, barabas_albert_m, resu
         for (u,v) in p:
             t_copy.add_edge(u, v)
             
-        algorithm_1_span_collector.append(graph.calculate_number_of_spanning_trees(t_copy))
+        print('algorithm1 res: ', p)
+        print('time: ', time.time() - global_start_time)
+        algorithm_1_span = graph.calculate_number_of_spanning_trees(t_copy)
+        algorithm_1_span_collector.append(algorithm_1_span)
         algorithm_1_edge_collector.append(p)
         algorithm_1_time.append((time.time() - start_time))
+        
+        print('algorithm1 span: ', algorithm_1_span)
+        print('time: ', time.time() - global_start_time)
+        
+        start_time = time.time()
+        q_2 = set(dif_3)
+        greedy_p = st_alg.greedy(t_copy_3, q_2, k)
+        greedy_span = graph.calculate_number_of_spanning_trees(t_copy)
+        greedy_span_collector.append(greedy_span)
+        greedy_edge_collector.append(greedy_p)
+        greedy_time.append((time.time() - start_time))
         
         
     report_data = {
@@ -99,18 +138,34 @@ def compare_algorithms(k, sample_size, barabasi_albert_n, barabas_albert_m, resu
         'algorithm_1_span_collector': algorithm_1_span_collector,
         'algorithm_1_edge_collector': algorithm_1_edge_collector,
         'algorithm_1_time': algorithm_1_time,
+        'greedy_span_collector': greedy_span_collector,
+        'greedy_edge_collector': greedy_edge_collector,
+        'greedy_time': greedy_time
     }
     
     df = pd.DataFrame(report_data)
     
     if not os.path.exists('improved_algorithm_result'):
         os.makedirs('improved_algorithm_result')
-    if not os.path.exists('improved_algorithm_result/' + str(k)):
-        os.makedirs('improved_algorithm_result/' + str(k))
-    df.to_csv('./improved_algorithm_result/' + str(k) + '/' + str(result_file) + '.csv', index=False)
+    if not os.path.exists('improved_algorithm_result/big_graphs'):
+        os.makedirs('improved_algorithm_result/big_graphs')
+    if not os.path.exists('improved_algorithm_result/big_graphs/' + str(k)):
+        os.makedirs('improved_algorithm_result/big_graphs/' + str(k))
+    df.to_csv('./improved_algorithm_result/big_graphs/' + str(k) + '/' + str(result_file) + '.csv', index=False)
+
+ 
+if __name__ == "__main__":
     
-#if __name__ == "__main__":
-compare_algorithms(3, 20, 15, 2, 'sample_20_barabasi_15_2')
-#compare_algorithms(3, 50, 15, 2, 'sample_20_barabasi_15_2')
-alg_analize.algorithm_evaluation('./improved_algorithm_result/3/sample_20_barabasi_15_2.csv', 'sample_20_eval', 3)
-#alg_analize.algorithm_evaluation('./improved_algorithm_result/3/sample_50_barabasi_15_2.csv', 'sample_50_eval', 3)
+    if (sys.argv):
+        k = int(sys.argv[1])
+        sample_size = int(sys.argv[2])
+        barabasi_n = int(sys.argv[3])
+        barabasi_m = int(sys.argv[4])
+        result_file = str(sys.argv[5])
+        report_file = str(sys.argv[6])
+    else:
+        print('Some parameters are missing')
+    
+    compare_algorithms(k, sample_size, barabasi_n, barabasi_m, result_file)
+    alg_analize.algorithm_evaluation(result_file, report_file, k)
+    
